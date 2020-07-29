d556e0dbc828a9f54d8bdfb06f72ea28db358de0
Steve Haslam <steve.haslam@timgroup.com>
2020-07-29 18:34:40 +0100

    Use jsonencode for role assumption policies

--- main.tf
+++ main.tf
@@ -47,18 +47,18 @@ resource "aws_s3_bucket" "test-tim-idea-attachments" {
 resource "aws_iam_role" "virus-scanner" {
   name = "virus-scanner"
   path = "/service-role/"
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-  {
-   "Action": "sts:AssumeRole",
-   "Effect": "Allow",
-   "Principal": { "Service": "lambda.amazonaws.com" }
-  }
- ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_policy" "test-tim-idea-attachments" {
@@ -120,18 +120,18 @@ resource "aws_s3_bucket_notification" "test-tim-idea-attachments" {
 resource "aws_iam_role" "virus-definitions-update" {
   name = "virus-definitions-update"
   path = "/service-role/"
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-  {
-   "Action": "sts:AssumeRole",
-   "Effect": "Allow",
-   "Principal": { "Service": "lambda.amazonaws.com" }
-  }
- ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_lambda_function" "virus-definitions-update" {
