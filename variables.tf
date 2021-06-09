ec4986173ea08714610572879593f26c14be29a5
Mehul Shah <mehul.shah@timgroup.com>
2021-06-09 16:41:19 +0100

    Passing in the meet webhook instead as slack is going away

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "slack_webhook_url" {
   description = "URL of Slack webhook to publish virus alerts to"
   type        = string
-  default     = "https://hooks.example.com/services/T025NKV7Y/BPURVP9TR/HmIdubHtWjD3B0cUpBFNRY5s"
+  default     = "https://iontradingcom.webhook.office.com/webhookb2/e79d6893-1796-43c9-8436-16b013d775c6@768fe7d4-ebee-41a7-9851-d5825ecdd396/IncomingWebhook/7ec135185dc44884947406fb420c16ed/377a4522-f214-47df-b616-ca917f253554"
 }
